bd03268a12690849fa53dd0f789caf25181a787c
Wkahmad12 <114064137+Wkahmad12@users.noreply.github.com>
2023-12-23 13:11:08 +0400

    final project
    sir usman final project

---
+++ kk.js
@@ -0,0 +1,8 @@
+
+    document.addEventListener("DOMContentLoaded", function () {
+        const header = document.querySelector("header");
+
+        window.addEventListener("scroll", function () {
+            header.classList.toggle("sticky", window.scrollY > 80);
+        });
+    });
